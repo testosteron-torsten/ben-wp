b20e1264106af2db8b1c8bebeb3a4512f61a16aa
Noah Frauenschuh <nfrauenschuh.mmt-b2021@fh-salzburg.ac.at>
2023-01-06 13:11:12 +0100

    try to fix js 2

--- public/wp-content/themes/ben/footer.php
+++ public/wp-content/themes/ben/footer.php
@@ -20,8 +20,34 @@
             h.classList.remove("shrunk");
         }
     });
+
+
+    const nav = document.querySelector("#mainnav");
+    const button = nav.querySelector("#hamburger");
+
+    button.addEventListener("click", e => {
+        console.log(button.classList)
+        if(button.classList.contains('closed')) {
+            button.classList.add('open');
+            button.classList.remove('closed');
+        } else {
+            button.classList.add('closed');
+            button.classList.remove('open');
+        }
+    });
+
+    const link = nav.querySelectorAll("a");
+    
+    for(var i = 0; i < link.length; i++) {
+        link[i].addEventListener("click" , e => {
+            console.log(button.classList)
+            button.classList.add('closed');
+            button.classList.remove('open');
+        })
+    }
+
 </script>
-<script src='/main.js'></script>
+<!-- <script src='/main.js'></script> -->
 <?php wp_footer(); ?>
 </body>
 </html>
